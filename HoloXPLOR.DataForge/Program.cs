2752d2689843aa5c49f1c37ae5d98862cc2801cc
Peter Dolkens <peter.dolkens@ddrit.com>
2016-02-24 00:28:53 +0000

    Command Line Version

---
+++ HoloXPLOR.DataForge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoloXPLOR.DataForge
+{
+    public static class Program
+    {
+        public static void Main(params String[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: HoloXPLOR.DataForge.exe [infile]");
+                Console.WriteLine();
+                Console.WriteLine("Converts an SC binary `xml` file into an actual XML file, and saves it as a .raw file in the original location");
+                return;
+            }
+
+            var xml = DataForgeSerializer.ReadFile(args[0]);
+            xml.Save(Path.ChangeExtension(args[0], "raw"));
+        }
+    }
+}
